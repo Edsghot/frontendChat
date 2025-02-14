2f98af66d01f7e63aa3a37c0e2f22e690cceb90b
edsghot <edsghot@gmail.com>
2025-02-14 11:28:35 -0500

    avance

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://5q3nk5t4-5001.brs.devtunnels.ms/api" : "/api",
   withCredentials: true,
 });
